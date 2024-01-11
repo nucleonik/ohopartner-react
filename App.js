78aeb0d15e5355f06881b00b61f47faf112ba1f1
nucleonik <asif.hossain@CBT.LCL>
2024-01-11 06:19:48 +0600

    modal change 2

--- App.js
+++ App.js
@@ -18,18 +18,12 @@ const transparent = 'rgba(0,0,0,0.5)';
 function renderModal() {
 
   return (
+   
     <Modal 
     visible={openModal} 
     animationType='none' 
     transparent={true}>
-    <TouchableOpacity
-          style={{
-            flex: 1,
-            width: '100%',
-            height: '100%'
-          }}
-          onPress={closeForgotPasswordModal}
-        >
+    
       <View style={{
         flex: 1,
         justifyContent: 'center',
@@ -40,16 +34,104 @@ function renderModal() {
           backgroundColor: 'white',
           padding: 15,
           width: '90%',
-          height: 500,
-          borderRadius: 10,
+          height: 350,
+          borderRadius: 8,
         }}>
-          <Text>Why did you forgot your password?</Text>
+          <View>
+            <View 
+            style={{
+              flexDirection: 'row',
+              marginTop: 23,
+              marginLeft: 10,
+            }}>
+              <Image
+              style={{
+                height: 32,
+                width: 32,
+              }}
+              source={require('../my-app/assets/password_icon_gradient.png')}/>
+              <Text style={{
+                fontSize: 22,
+                fontWeight: 600,
+                marginLeft: 10,
+              }}>Request New Password</Text>
+            </View>
+            <Text
+            style={{
+              fontSize: 17,
+              fontWeight: 400,
+              lineHeight: 30,
+              marginLeft: 10,
+              marginTop: 15,
+            }}
+            >Please enter your registered email below and tap the submit button. We will send you a new password in your email.
+            </Text>
+            <View style={{
+              flexDirection: 'row'
+            }}>
+            <TextInput 
+          style={{
+          height: 50, 
+          borderColor: '#eee', 
+          borderTopWidth: 1,
+          borderBottomWidth: 1,
+          borderLeftWidth: 1, 
+          color:'#595959',
+          width: 255,
+          paddingLeft: 15,
+          marginLeft: 10,
+          marginTop: 20,
+          
+        }}
+        placeholder='Enter your email'
+
+        autoCapitalize='none'
+        autoCorrect={false}>
+          
+        </TextInput>
+        <View
+        style={{
+          justifyContent: 'center',
+          alignContent: 'center',
+          align: 'center',
+          borderTopWidth: 1,
+          borderBottomWidth: 1,
+          borderRightWidth: 1,
+          borderColor: '#eee',
+          width: 50,
+          marginTop: 20,
+        }}>
+        <Image source={require('../my-app/assets/email_icon.png')}/>
+        </View></View>
+
+          </View>
+          <View style={{
+            flexDirection: 'row',
+            marginLeft: 10,
+            alignItems: 'flex-end',
+            justifyContent: 'flex-end',
+            width: 305,
+            marginTop: 40,
+           
+          }}>
           <TouchableOpacity onPress={closeForgotPasswordModal}>
-              <Text>Close Modal</Text>
+              <Text style={{
+                fontSize: 20,
+                marginRight: 20,
+                letterSpacing: 2,
+              }}>Cancel</Text>
             </TouchableOpacity>
+            <TouchableOpacity>
+            <Text style={{
+                fontSize: 20,
+                letterSpacing: 2,
+                color: '#BE2653'
+              }}>Submit</Text>
+              </TouchableOpacity>
+            </View>
         </View>
       </View>
-      </TouchableOpacity>  
+      
     </Modal>
   )
   
